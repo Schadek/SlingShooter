e738ef7b16f9306f2dbc89a98ae157682f1dc3ad
Thomas Valentin Klink <thomasvalentinklink@gmail.com>
2015-07-21 18:50:57 +0200

    Complete wipe for testing purposes

--- Assets/Scripts/GlobalSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class GlobalSettings
-{
-    public static float woodMass = 8f;
-    public static float iceMass = 5f;
-    public static float stoneMass = 12f;
-}
